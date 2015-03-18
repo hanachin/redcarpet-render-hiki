dd7c76a37262a6cfecec356ff73c0e0ed772188e
Seiei Higa <hanachin@gmail.com>
2015-03-18 21:30:11 +0900

    Fix test

--- test/test_recarpet-render-hiki.rb
+++ test/test_recarpet-render-hiki.rb
@@ -5,21 +5,22 @@ require 'redcarpet/render/hiki'
 class TestRecarpetRenderHiki < Test::Unit::TestCase
   def test_render_markdown_as_hiki
     renderer = Redcarpet::Render::Hiki
-    markdown = Redcarpet::Markdown.new(renderer.new)
-    assert_equal(markdown.render(<<MARKDOWN), <<HIKI)
+    rc_markdown = Redcarpet::Markdown.new(renderer)
 
+    markdown = <<MARKDOWN
 [redcarpet-render-hiki](https://github.com/hanachin/redcarpet-render-hiki)で、
 Markdownをhiki記法に変換したい。
 なぜなら、Markdownで書かれた記事をhikiに変換できるとるびまの編集の手間が減って、
 世界がちょっと幸せになりそうだからだ。
-
 MARKDOWN
 
-[[redcarpet-render-hiki|https://github.com/hanachin/redcarpet-render-hiki]]
+    hiki = <<HIKI
+[[redcarpet-render-hiki|https://github.com/hanachin/redcarpet-render-hiki]]で、
 Markdownをhiki記法に変換したい。
 なぜなら、Markdownで書かれた記事をhikiに変換できるとるびまの編集の手間が減って、
 世界がちょっと幸せになりそうだからだ。
-
 HIKI
+
+    assert_equal(rc_markdown.render(markdown), hiki)
   end
 end
