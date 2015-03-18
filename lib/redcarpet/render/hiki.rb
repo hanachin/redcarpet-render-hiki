ffb2d3fad52260454510b535b757493d71902c26
Seiei Higa <hanachin@gmail.com>
2015-03-18 21:31:13 +0900

    render link as hiki success! yey!

--- lib/redcarpet/render/hiki.rb
+++ lib/redcarpet/render/hiki.rb
@@ -3,7 +3,14 @@ require "redcarpet/render/hiki/version"
 module Redcarpet
   module Render
     class Hiki < Base
-      # Your code goes here...
+      def link(link, title, content)
+        # FIXME I don't care about title
+        "[[#{content}|#{link}]]"
+      end
+
+      def paragraph(text)
+        text + $/
+      end
     end
   end
 end
