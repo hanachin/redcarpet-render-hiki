7c849f9d3eb0cf8820e73d07cca30f715d2758c2
Seiei Higa <hanachin@gmail.com>
2015-03-18 20:11:30 +0900

    bundle gem redcarpet-render-hiki

---
+++ lib/redcarpet/render/hiki.rb
@@ -0,0 +1,9 @@
+require "redcarpet/render/hiki/version"
+
+module Redcarpet
+  module Render
+    module Hiki
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/redcarpet/render/hiki/version.rb
@@ -0,0 +1,7 @@
+module Redcarpet
+  module Render
+    module Hiki
+      VERSION = "0.0.1"
+    end
+  end
+end
